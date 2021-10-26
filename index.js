66fcf01c5a37567bdf294905c99ffec7165aeace
Gayathri Devi Nagalapuram <gayathri1462@gmail.com>
2021-10-26 18:36:23 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+$.ajax("/campushiring.csv", {
+    success: function(data) {
+        var csv = d3.csv.parse(data);
+        console.table(csv);
+    }
+});
+var chart = new dimple.chart(svg, csv);
+chart.addCategoryAxis("x", "Company Name");
+chart.addMeasureAxis("y", "No. of Offers");
+chart.addSeries(null, dimple.plot.bar);
+chart.draw();
